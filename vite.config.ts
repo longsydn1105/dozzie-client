6c3f052c43d9066adc91d3d4febbb6e2b330b0f3
longsydn1105 <longsydn1105@gmai.com>
2025-11-25 00:18:02 +0700

    Fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+    plugins: [tailwindcss()],
+    base: './',
+    build: {
+        outDir: 'dist',
+    },
+});
