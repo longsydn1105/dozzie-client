a76f1940f73b89dde12cf1719fe86a7bfa5c4228
longsydn1105 <longsydn1105@gmai.com>
2025-11-25 00:33:15 +0700

    Fix config to JS

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [tailwindcss()],
     base: './',
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'login.html'),
+                register: resolve(__dirname, 'register.html'),
+                booking: resolve(__dirname, 'book-now.html'),
+                blog: resolve(__dirname, 'blog.html'),
+                faq: resolve(__dirname, 'faq.html'),
+            },
+        },
     },
 });
