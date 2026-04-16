09fda368bc76eb83e9a2ec0b8199e00d882dd866
longsydn1105 <longsydn1105@gmail.com>
2026-04-16 17:31:38 +0700

    Add vite.config.js to build all pages

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,18 @@ export default defineConfig({
                 faq: resolve(__dirname, 'faq.html'),
                 about: resolve(__dirname, 'about.html'),
                 membership: resolve(__dirname, 'membership.html'),
-                features: resolve(__dirname, 'features.html')
+                features: resolve(__dirname, 'features.html'),
+                myBooking: resolve(__dirname, 'my-booking.html'),
+                payment: resolve(__dirname, 'payment.html'),
+
+                // --- KHU VỰC ADMIN (TẤT CẢ FILE TRONG THƯ MỤC ADMIN) ---
+                adminDashboard: resolve(__dirname, 'admin/dashboard.html'),
+                adminBookings: resolve(__dirname, 'admin/bookings.html'),
+                adminInvoices: resolve(__dirname, 'admin/invoices.html'),
+                adminRoomDetail: resolve(__dirname, 'admin/room-detail.html'),
+                adminRooms: resolve(__dirname, 'admin/rooms.html'),
+                adminServicePackage: resolve(__dirname,'admin/servicepackage.html',),
+                adminUsers: resolve(__dirname, 'admin/users.html'),
             },
         },
     },
